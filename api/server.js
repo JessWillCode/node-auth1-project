65ae7350910d0832b99f45bf49959490157d26ab
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-02-22 16:20:05 -0500

    updated session

--- api/server.js
+++ api/server.js
@@ -2,28 +2,36 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require('express-session');
-
-const usersRouter = require('./users/users-router.js')
-const authRouter = require('./auth/auth-router.js')
+const Store = require('connect-session-knex')(session);
+const knex = require('../data/db-config');
+const usersRouter = require('./users/users-router.js');
+const authRouter = require('./auth/auth-router.js');
 
 const server = express();
+
+server.use(session({
+  name: 'chocolatechip', 
+  secret: 'shhhhhh', 
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,
+    createtable: true,
+    clearInterval: 1000 * 60 * 10,
+    tablename: 'sessions',
+    sidfieldname: 'sid'
+  }), 
+  cookie: {
+    macAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+    // sameSite: 'none' - for third party cookies
+  }
+}))
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
-server.use(session({
-  name: 'chocolatechip', 
-  secret: 'make it long and random',
-  cookie: {
-    maxAge: 1000 * 60 * 60,
-    secure: false, 
-    httpOnly: false, 
-  },
-  rolling: true, 
-  resave: false, 
-  saveUninitialized: false 
-}))
 
 server.use('/api/users', usersRouter)
 server.use('/api/auth', authRouter)
